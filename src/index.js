c08231a50a78f0a73e69c947c671fb97e5370803
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-09-16 21:47:57 +0300

    Install Express and add Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Test")
+import express from "express";
+
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+})
+
+app.listen(5000, () => console.log("Server is listening on http://localhost:5000..."));
