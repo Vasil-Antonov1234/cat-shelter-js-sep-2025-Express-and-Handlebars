a2eaabbd19466f04c2e742526c8e26c69ea1d54c
Vasil Antonov <vaskoantonov1234@gmail.com>
2025-09-21 22:42:15 +0200

    Add files via upload

---
+++ src/services/catService.js
@@ -0,0 +1,29 @@
+import Cat from "../models/Cat.js";
+
+
+export default {
+    getAll(filter) {
+        return Cat.find();
+    },
+
+    getCatById(catId) {
+        return Cat.findOne({_id: catId});
+    },
+
+
+    create(catData) {
+        const cat = new Cat(catData);
+
+        cat.save();
+        return cat;
+    },
+
+    update(catId, newCatData) {
+
+        const cat = new Cat(newCatData);
+
+        cat.save({_id: catId});
+
+        return cat;
+    }
+}
